0efd9adcf35949743b19046b5b7455d9d52e7315
User <tjasur@gmail.com>
2024-01-30 15:23:27 +0500

    drf_1

--- my_api/urls.py
+++ my_api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from .views import UserViewSet
 
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', user, name='user')
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
